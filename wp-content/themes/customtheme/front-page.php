0ccaa7c7b39d0992caaacf683df7580dcdd8d696
Vialli Gonsalves <gonsalvesvialli06@gmail.com>
2023-03-14 18:41:38 +0530

    initial commit

--- wp-content/themes/customtheme/front-page.php
+++ wp-content/themes/customtheme/front-page.php
@@ -8,4 +8,165 @@
 
 <?php endwhile; else: endif;?>
 
+
+<div class="button-section text-center" >
+                <button class="google-btn" type="button">
+                    Sign up with Google
+                </button>
+
+                <button class="email-btn" type="button">Sign up with Gmail</button>
+
+            <p>Free seven days trial. Get started</p>
+</div>
+
+<hr class="dotted-border">
+
+<div id="part-two">
+    <div class="container">
+        <div class="row">
+            <div class="col-6">abcb</div>
+
+            <div class="col-6">
+                <p>Here's how it works</p>
+                <ul>
+                    <li>Brainstorming ideas</li>
+                    <li>Business intelligence</li>
+                    <li>Business plans</li>
+                    <li>Autopilot</li>
+                </ul>
+                <p><u>View all features</u></p>
+            </div>
+        </div>
+    </div>
+
+    <hr class="dotted-border">
+
+<div id="part-three">
+    <div class="container text-center">
+        <img src=<?php echo get_template_directory_uri(); ?>'/images/time.png' class="img-fluid" width="50">
+        <h1>Save time on market research and competitive analysis</h1>
+        <p id="p-3">Brainstorm startup ideas automatically, reducing time spent on market research and competitive analysis from hours to minutes. Access to a stunning business model canvas database with thousands of examples that will inspire you, create and edit <b>unique business plans</b> using the mash-up method, export your projects easily.</p>
+        <div class="button-section">
+            <button class="greenbtn" type="button">
+                See how vizologi works 
+            </button>
+
+            <button class="purplebtn" type="button">View all features ></button>
+        </div>
+    </div>
+
+</div>
+
+<hr class="dotted-border">
+
+<div id="part-four">
+    <div class="container text-center">
+    <img src=<?php echo get_template_directory_uri(); ?>'/images/hand.svg' class="img-fluid" width="180" height="67">
+        <!-- <img src=<?php echo get_template_directory_uri(); ?>"/images/hand.svg"> -->
+        <p id="p-4">Vizologi has helped thousands of people complete</p>
+        <h2>over 1.5 million questions in 25+ thousand projects</h2>
+        <div class="row">
+            <div class="col-2">
+                <!-- <img src="designit.webp"> -->
+                <img src=<?php echo get_template_directory_uri(); ?>'/images/designit.webp' class="img-fluid" width="130" height="65">
+            </div>
+            <div class="col-2">
+                <!-- <img src="apple.webp"> -->
+                <img src=<?php echo get_template_directory_uri(); ?>'/images/apple.webp' class="img-fluid" width="130" height="65">
+            </div>
+            <div class="col-2">
+                <!-- <img src="amazon.webp"> -->
+                <img src=<?php echo get_template_directory_uri(); ?>'/images/amazon.webp' class="img-fluid" width="130" height="65">
+            </div>
+            <div class="col-2">
+                <!-- <img src="autodesk.webp"> -->
+                <img src=<?php echo get_template_directory_uri(); ?>'/images/autodesk.webp' class="img-fluid" width="130" height="65">
+            </div>
+            <div class="col-2">
+                <!-- <img src="descript.webp"> -->
+                <img src=<?php echo get_template_directory_uri(); ?>'/images/descript.webp' class="img-fluid" width="130" height="65">
+            </div>
+            <div class="col-2">
+                <!-- <img src="ey.webp"> -->
+                <img src=<?php echo get_template_directory_uri(); ?>'/images/ey.webp' class="img-fluid" width="130" height="65">
+            </div>
+        </div>
+        <div class="people">
+        <div class="row">
+            <div class="testimonial stratergists">
+                <img src=<?php echo get_template_directory_uri(); ?>'/images/testimonial-strategist.webp' class="img-fluid" width="250" height="250">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="testimonial creatives">
+            <img src=<?php echo get_template_directory_uri(); ?>'/images/testimonial-creatives.webp' class="img-fluid" width="250" height="250">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="testimonial consultants">
+            <img src=<?php echo get_template_directory_uri(); ?>'/images/testimonial-consultants.webp' class="img-fluid" width="250" height="250">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="testimonial entrepreuners">
+            <img src=<?php echo get_template_directory_uri(); ?>'/images/testimonial-entrepreneurs.webp' class="img-fluid" width="250" height="250">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="testimonial managers">
+            <img src=<?php echo get_template_directory_uri(); ?>'/images/testimonial-managers.webp' class="img-fluid" width="250" height="250">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="testimonial students">
+            <img src=<?php echo get_template_directory_uri(); ?>'/images/testimonial-students.png' class="img-fluid" width="250" height="250">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+</div>
+            <!-- <div class="row">
+                <div class="col-12">
+                    <button class="btn">strategist</div>
+                    <button class="btn">strategist</div>
+                    <button class="btn">strategist</div>
+                    <button class="btn">strategist</div>
+                    <button class="btn">strategist</div>
+                    <button class="btn">strategist</div>
+                </div>
+            </div> -->
+        </div>
+    </div>
+</div>
+
+<hr class="dotted-border">
+
+<div class="part-five">
+    <div class="container">
+        <h1>Transform a cost into revenue</h1>
+        <p>For every $1 invested in Vizologi you generate over $75 with new ideas.</p>
+        <ul class="nav justify-content-center">
+            <li>Monthly</li>
+            <li>Yearly</li>
+        </ul>
+        <div></div>
+    </div>
+</div>
+
+<hr class="dotted-border">
+
+<div class="part-six ">
+    <div class="container text-center">
+        <h1 class="p-6">Craft a Business Plan FAST using AI</h1>
+        <p class="new-abc">It’s FREE and EASY to get started. Two things people love.</p>
+        <button class="google-btn">Sign up with google account</button>
+        <p>OR</p>
+        <input type="text" class="newtext" placeholder="Your email">
+        <br>
+        <button class="getstarted-btn">Get started</button>
+        <p>Free 7 days trial - Get started in seconds</p>
+    </div>
+</div>
+
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
+
+</div>
+
 <?php get_footer();?>
--- wp-content/themes/customtheme/header.php
+++ wp-content/themes/customtheme/header.php
@@ -37,14 +37,220 @@
             text-align: center;
         }
 
+        .google-btn {
+            margin-right: 25px;
+            background: #518ef8;
+            display: inline-flex;
+            line-height: 25px;
+            border: 2px solid #518ef8;
+            border-radius: 35px;
+            margin-bottom: 20px;
+            position: relative;
+            text-align: center;
+            font-size: 20px;
+            font-family: roobert, sans-serif;
+            color: aliceblue;
+            padding: 20px 30px;
+        }
+
+        .email-btn {
+            display: inline-flex;
+            line-height: 25px;
+            background: #fff;
+            border: 2px solid #3c3c3c;
+            border-radius: 35px;
+            margin-bottom: 20px;
+            position: relative;
+            text-align: center;
+            font-size: 20px;
+            padding: 20px 30px;
+        }
+
+        .new-tag {
+            color: #F47574;
+            cursor: pointer;
+            background-color: transparent;
+            border: 2px solid #F47574;
+            border-radius: 50px;
+        }
+
+        #color-over {
+            background-color: yellow;
+        }
+
+        .para-content {
+            margin: 35px auto 60px;
+            text-align: center;
+            font-size: 20px;
+            line-height: 25px;
+        }
+
+        .main-header-content {
+            margin-top: 56px;
+            color: #3c3c3c;
+            position: relative;
+            font-size: 50px;
+            font-family: roobert, sans-serif;
+            text-align: center;
+        }
+
+        .google-btn {
+            margin-right: 25px;
+            background: #518ef8;
+            display: inline-flex;
+            line-height: 25px;
+            border: 2px solid #518ef8;
+            border-radius: 35px;
+            margin-bottom: 20px;
+            position: relative;
+            text-align: center;
+            font-size: 20px;
+            font-family: roobert, sans-serif;
+            color: aliceblue;
+            padding: 20px 30px;
+        }
+
+        .getstarted-btn {
+            font-size: 20px;
+            margin-right: 25px;
+            padding: 20px 30px;
+            background-color: #F47574;
+            border-color: #F47574;
+            color: #fff;
+            font-weight: bold;
+            font-family: "Roobert", sans-serif;
+            padding: 0 30px;
+            border-radius: 40px;
+            z-index: 8;
+            height: 70px;
+            line-height: 66px;
+            width: 330px;
+        }
+
+        .newtext {
+            margin-right: 25px;
+            background: #fff;
+            height: 50px;
+            line-height: 50px;
+            border: 2px solid #3c3c3c;
+            border-radius: 35px;
+            margin-bottom: 20px;
+            position: relative;
+            text-align: center;
+            font-size: 20px;
+            font-family: roobert, sans-serif;
+            color: aliceblue;
+            padding: 20px 30px;
+        }
+
+        .greenbtn {
+            border-color: #88c7a8;
+            background: #88c7a8;
+            color: #fff;
+            margin-right: 25px;
+            height: 70px;
+            line-height: 50px;
+            border-radius: 35px;
+            margin-bottom: 20px;
+            position: relative;
+            text-align: center;
+            font-size: 20px;
+            font-family: roobert, sans-serif;
+            padding: 0px 30px;
+        }
+
+        .purplebtn {
+            border-color: #645BC9;
+            background: #645BC9;
+            color: #fff;
+            margin-right: 25px;
+            height: 70px;
+            line-height: 50px;
+            border-radius: 35px;
+            margin-bottom: 20px;
+            position: relative;
+            text-align: center;
+            font-size: 20px;
+            font-family: roobert, sans-serif;
+            padding: 0px 30px;
+        }
+
+        .email-btn {
+            display: inline-flex;
+            line-height: 25px;
+            background: #fff;
+            border: 2px solid #3c3c3c;
+            border-radius: 35px;
+            margin-bottom: 20px;
+            position: relative;
+            text-align: center;
+            font-size: 20px;
+            padding: 20px 30px;
+        }
+
+        .text-container {
+            text-align: center;
+        }
+
+        h1 {
+            line-height: 72px;
+            color: #3c3c3c;
+            position: relative;
+            font-size: 50px;
+            font-weight: bold;
+        }
+
+        .column1 {
+            max-width: 66.66%;
+        }
+
+        .column2 {
+            max-width: 33.33%;
+        }
+
+        .dotted-border {
+            height: 5px;
+            background-image: linear-gradient(to right, #000 30%, rgba(255, 255, 255, 0) 0%);
+            background-position: bottom;
+            background-size: 13px 3px;
+            background-repeat: repeat-x;
+        }
+
+        .new-abc {
+            font-size: 20px;
+            line-height: 20px;
+        }
+
+        #p-3 {
+            margin-top: 20px;
+            font-size: 20px;
+        }
+
+        #p-4 {
+            font-size: 20px;
+        }
+
+        .dotted-border {
+            background: url(<?php echo get_template_directory_uri(); ?>/images/dotted-border.png) center center;
+            background-size: cover;
+            border: none;
+            margin: 0;
+            width: 100%;
+            height: 2px;
+            display: inline-block;
+            vertical-align: middle;
+        }
+
 
     </style>
     <header>
     <div class="container">
-        <div class="row">
-        <?php if(has_post_thumbnail()): ?>
+            <div class="container-fluid">
+                <!-- <img src=<?php echo get_template_directory_uri(); ?>"/images/color-logo.png" width="150"> -->
+                <img src=<?php echo get_template_directory_uri(); ?>'/images/color-logo.png' class="img-fluid" width="120" height="45">
+        <!-- <?php if(has_post_thumbnail()): ?>
         <img src="<?php the_post_thumbnail_url('smallest');?>" >
-    <?php endif;?>
+    <?php endif;?> -->
         <?php wp_nav_menu(
         array(
             'theme_location' => 'top-menu',
